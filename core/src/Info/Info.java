a588b3e9733565b31b1cf3c4ce4836d951fb20a3
Bombazok <sergeipiskarev@yandex.ru>
2019-11-07 20:31:39 +0300

    Back to good version;

---
+++ core/src/Info/Info.java
@@ -0,0 +1,15 @@
+package Info;
+
+import com.badlogic.gdx.Gdx;
+
+public interface Info {
+    float WIDTH = Gdx.graphics.getWidth();
+    float HEIGHT = Gdx.graphics.getHeight();
+    float RIGHT_EDGE_X = 400;
+    float LEFT_EDGE_X = 80;
+    float BOTTOM_EDGE_Y = 134;
+    float TOP_EDGE_Y = 774;
+    float SQ_W = 32;//
+    float CENTER_X = 240;
+    float FRAMES_PER_SECOND = 60.098f;
+}
